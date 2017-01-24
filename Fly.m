942b56d477a886fa2369ec7a2a1adef54c9b9aa8
deronmonta <deronmonta@gmail.com>
2017-01-25 00:21:14 +0800

    Class def
    Added some functions to method

--- Fly.m
+++ Fly.m
@@ -4,32 +4,42 @@
 
 classdef Fly
 
-  properties
-    filename;
-    id;
-    date;
-    genotype;
-    desiccation_hr;
-    plate_center;
-    plate_radius;
-    tzone_inner_radius;%tzone stands for target zone
-    tzone_mid_radius;
-    tzone_outer_radius;
-    center;
-    dis2center;
-    speed;
-    zone;%the posistion status of fly in each frame
-    dzone_inner_radius;%dzone stands for decision zone
-    dzone_outer_radius;
-    file_dir;
+    properties
+      filename;
+      id;
+      date;
+      genotype;
+      wholepos;
+      desiccation_hr;
+      plate_center;
+      plate_radius;
+      tzone_inner_radius;%tzone stands for target zone
+      tzone_mid_radius;
+      tzone_outer_radius;
+      center;
+      dis2center;
+      speed;
+      zone;%the posistion status of fly in each frame
+      dzone_inner_radius;%dzone stands for decision zone
+      dzone_outer_radius;
+      file_dir;
   end
 
 
   methods
+    function  self = Fly(tracks)%This is the constructor of fly object
 
-  function  obj = Fly(track)
+      self.id = track;
 
-    obj.id = track;
+    end
+
+    function self = correction(self)
+    end
+
+    function self = zoneidentify(self)
+    end
+
+    function self = displayresults(self)
 
     end
 
