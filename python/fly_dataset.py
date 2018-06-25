fa021c881ce0a6efa8037065f63ec1a2dda72b23
deronmonta <deronmonta@gmail.com>
2018-06-24 21:40:14 -0700

    Directory cleanup and pytorch
    Initialzing pytorch implementation, create pytorch datasets

---
+++ python/fly_dataset.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import torch.utils.data as data
+from glob import glob
+
+class FlyDataSet(data.Dataset):
+
+    def __init__(self):
+        '''
+        '''
+
+    def __getitem__(self,index):
+
+    def __len__(self):
+        
+        
